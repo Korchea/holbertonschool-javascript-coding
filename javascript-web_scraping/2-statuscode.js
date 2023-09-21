aeda204f6c135bff247313e88aad82987afa9bd3
Korchea <6334@holbertonstudents.com>
2023-09-21 11:25:23 -0700

    Task 2 completed

--- javascript-web_scraping/2-statuscode.js
+++ javascript-web_scraping/2-statuscode.js
@@ -1,6 +1,10 @@
 #!/usr/bin/node
 
 const request = require('request');
-request.get(process.argv[2], (error, response) => {
+request.get(process.argv[2], function (error, response) {
+  if (error) {
+    console.log('code:', response.statusCode);
+    return;
+  }
   console.log('code:', response.statusCode);
 });
