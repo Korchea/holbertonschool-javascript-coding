709ebcab0860ab75094e7d56ecc2468c7abb7f81
Korchea <6334@holbertonstudents.com>
2023-09-22 13:38:58 -0700

    Task 6 completed

--- javascript-web_scraping/6-completed_tasks.js
+++ javascript-web_scraping/6-completed_tasks.js
@@ -2,19 +2,27 @@
 
 const request = require('request');
 request(process.argv[2], function (error, response) {
-  if (error) {
-    console.error(error);
-  } else {
-    const JsonBody = JSON.parse(response.body);
-    const comTask = {};
-    for (const user of JsonBody) {
-      comTask[user.userId] = 0;
+    if (error) {
+        console.error(error);
+    } else {
+        const JsonBody = JSON.parse(response.body);
+        const comTask = {};
+        let maxId = 0;
+        for (const user of JsonBody) {
+            comTask[user.userId] = 0;
+            maxId = user.userId;
+        }
+        for (const user of JsonBody) {
+            if (user.completed === true) {
+                comTask[user.userId]++;
+            }
+        }
+        for (let i = 0; i <= maxId; i++) {
+            if (comTask[i] !== 0) {
+                console.log(comTask);
+                return;
+            }
+        }
+        console.log({});
     }
-    for (const user of JsonBody) {
-      if (user.completed === true) {
-        comTask[user.userId]++;
-      }
-    }
-    console.log(comTask);
-  }
 });
